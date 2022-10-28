d59b88c7b38b33397a9c88544e47a996a25bc770
Brandon <brandonlevis84@gmail.com>
2022-10-28 13:04:59 -0400

    Add project files.

---
+++ GUI_FrontEnd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GUI_FrontEnd.Startup))]
+namespace GUI_FrontEnd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
